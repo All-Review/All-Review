7b26da3a531fde11e41c7d5835996071ca82d04c
위징 <@59.0.61.68>
2024-08-27 18:07:27 +0900

    Merge branch 'main' of https://github.com/All-Review/All-Review.git into main

--- All_Review/src/main/java/util/DatabaseUtil.java
+++ All_Review/src/main/java/util/DatabaseUtil.java
@@ -7,9 +7,9 @@ public class DatabaseUtil {
 	
 	public static Connection getConnection() {
 		try {
-			String dbURL = "jdbc:mysql://localhost:3306/temp"; // 데이터베이스 이름
-			String dbID = "root";
-			String dbPassword = "2103";
+			String dbURL = "jdbc:mysql://121.148.107.112:3306/test"; // 데이터베이스 이름
+			String dbID = "yuns";
+			String dbPassword = "9899";
 			Class.forName("com.mysql.jdbc.Driver");
 			return DriverManager.getConnection(dbURL, dbID, dbPassword);
 		} catch (Exception e) {
