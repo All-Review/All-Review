98d0292af234ec302cdd4f64504b8cebe19288f1
WIN-TNQDE2O25PD\yuns <yuns8708@gmail.com>
2024-09-02 17:44:13 +0900

    댓글 컬럼 이름 변경

--- All_Review/src/main/java/comment/PostComment.java
+++ All_Review/src/main/java/comment/PostComment.java
@@ -1,51 +1,51 @@
 package comment;
 
 public class PostComment {
-	private int postNum; // 몇 번 게시물의 댓글인지
-	private int commentNum; // 댓글 고유번호
+	private int postIndex; // 몇 번 게시물의 댓글인지
+	private int commentIndex; // 댓글 고유번호
 	private String userId; // 사용자 아이디(고유)
 	private String nickname; // 사용자 닉네임
-	private String userImgUrl; // 사용자 프로필사진 주소
+	private String userProfileImage; // 사용자 프로필사진 주소
 	private String commentContent; // 댓글 내용
 	private double commentRate; // 댓글 별점
 	private String commentCreateAt; // 댓글 생성 시간
 	
 	public PostComment() {}
 	
-	public PostComment(int postNum, String userId, String nickname, String userImgUrl, String commentContent, double commentRate, String commentCreatedAt) {
-		this.postNum = postNum;
+	public PostComment(int postNum, String userId, String nickname, String userProfileImage, String commentContent, double commentRate, String commentCreatedAt) {
+		this.postIndex = postNum;
 		this.userId = userId;
 		this.nickname = nickname;
-		this.userImgUrl = userImgUrl;
+		this.userProfileImage = userProfileImage;
 		this.commentContent = commentContent;
 		this.commentRate = commentRate;
 		this.commentCreateAt = commentCreatedAt;
 	}
 	
 	// 댓글 읽어오기 전용
-	public PostComment(int postNum, int commentNum, String userId, String nickname, String userImgUrl, String commentContent, double commentRate, String commentCreatedAt) {
-		this.postNum = postNum;
-		this.commentNum = commentNum;
+	public PostComment(int postNum, int commentIndex, String userId, String nickname, String userProfileImage, String commentContent, double commentRate, String commentCreatedAt) {
+		this.postIndex = postNum;
+		this.commentIndex = commentIndex;
 		this.userId = userId;
 		this.nickname = nickname;
-		this.userImgUrl = userImgUrl;
+		this.userProfileImage = userProfileImage;
 		this.commentContent = commentContent;
 		this.commentRate = commentRate;
 		this.commentCreateAt = commentCreatedAt;
 	}
 	
-	public int getPostNum() {
-		return postNum;
+	public int getPostIndex() {
+		return postIndex;
 	}
-	public void setPostNum(int postNum) {
-		this.postNum = postNum;
+	public void setPostIndex(int postNum) {
+		this.postIndex = postNum;
 	}
 
-	public int getCommentNum() {
-		return commentNum;
+	public int getCommentIndex() {
+		return commentIndex;
 	}
-	public void setCommentNum(int commentNum) {
-		this.commentNum = commentNum;
+	public void setCommentIndex(int commentIndex) {
+		this.commentIndex = commentIndex;
 	}
 
 	public String getUserId() {
@@ -62,11 +62,11 @@ public class PostComment {
 		this.nickname = nickname;
 	}
 	
-	public String getUserImgUrl() {
-		return userImgUrl;
+	public String getUserProfileImage() {
+		return userProfileImage;
 	}
-	public void setUserImgUrl(String userImgUrl) {
-		this.userImgUrl = userImgUrl;
+	public void setUserProfileImage(String userProfileImage) {
+		this.userProfileImage = userProfileImage;
 	}
 	
 	public String getCommentContent() {
